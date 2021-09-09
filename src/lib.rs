f321ef4bb662d37a5411938d3ccdbeecde169f16
Sirui Mu <msrlancern@gmail.com>
2021-09-09 10:07:09 +0800

    Add comment documentation of code

--- src/lib.rs
+++ src/lib.rs
@@ -13,17 +13,24 @@ use core::fmt::{Debug, Display, Formatter};
 use core::ops::{Deref, DerefMut};
 use core::ptr::NonNull;
 
+/// A type-erased allocation arena with proper dropping.
 pub struct Arena {
     objects: RefCell<Vec<ArenaBox>>,
 }
 
 impl Arena {
+    /// Create a new arena.
     pub fn new() -> Self {
         Self {
             objects: RefCell::new(Vec::new()),
         }
     }
 
+    /// Allocate and initialize a new value in the arena.
+    ///
+    /// This function returns a safe wrapper around a mutable reference to the allocated value. When
+    /// being `Deref`-ed, it performs safety checks to ensure that the referenced value has not been
+    /// dropped.
     pub fn alloc<'s, 'a, T>(&'s self, value: T) -> AllocMut<'s, T>
     where
         's: 'a,
@@ -40,6 +47,11 @@ impl Arena {
         }
     }
 
+    /// Allocate and initialize a new value in the arena.
+    ///
+    /// This function is unsafe in the manner that a raw reference is returned rather than a safe
+    /// wrapper that checks the value has not been dropped when `Deref`-ed. This may lead to
+    /// potential use-after-free vulnerabilities as described in the crate-level documentation.
     pub unsafe fn alloc_unchecked<'s, 'a, T>(&'s self, value: T) -> &'s mut T
     where
         's: 'a,
@@ -60,30 +72,66 @@ impl Drop for Arena {
     }
 }
 
+/// A safe wrapper around a mutable reference to a value allocated in an arena.
+///
+/// This wrapper type can be `Deref`-ed to the allocated type. When being `Deref`-ed, this wrapper
+/// checks that the referenced value has not been dropped due to the dropping of the arena. For more
+/// explanation about why this could happen, you can refer to the crate-level documentation.
 pub struct AllocMut<'a, T: ?Sized> {
     value: &'a mut T,
     dropped: Rc<Cell<bool>>,
 }
 
 impl<'a, T: ?Sized> AllocMut<'a, T> {
+    /// Get an immutable reference to the allocated value.
+    ///
+    /// This function panics if the referenced value has been dropped.
     pub fn get(&self) -> &T {
         self.ensure_not_dropped();
         self.value
     }
 
+    /// Get a mutable reference to the allocated value.
+    ///
+    /// This function panics if the referenced value has been dropped.
     pub fn get_mut(&mut self) -> &mut T {
         self.ensure_not_dropped();
         self.value
     }
 
+    /// Get an immutable reference to the allocated value, without safety checks.
+    pub unsafe fn get_unchecked(&self) -> &T {
+        self.value
+    }
+
+    /// Get a mutable reference to the allocated value, without safety checks.
+    //noinspection RsSelfConvention
+    pub unsafe fn get_mut_unchecked(&mut self) -> &mut T {
+        self.value
+    }
+
+    /// Determine whether the referenced value has been dropped.
     pub fn dropped(&self) -> bool {
         self.dropped.get()
     }
 
-    pub fn leak(self) -> &'a mut T {
+    /// Consume this safety wrapper and leak the mutable reference to the allocated value.
+    ///
+    /// This function panics if the referenced value has been dropped.
+    pub unsafe fn leak(self) -> &'a mut T {
+        self.ensure_not_dropped();
         self.value
     }
 
+    /// Consume this safety wrapper and leak the mutable reference to the allocated value, without
+    /// safety checks.
+    pub unsafe fn leak_unchecked(self) -> &'a mut T {
+        self.value
+    }
+
+    /// Ensure that the referenced value has not been dropped.
+    ///
+    /// This function panics if the referenced value has been dropped.
     fn ensure_not_dropped(&self) {
         assert!(
             !self.dropped(),
@@ -148,13 +196,28 @@ where
     }
 }
 
+/// A type-erased smart pointer to an arena-allocated value.
+///
+/// The smart pointer will properly drop the allocated value upon the dropping of the arena.
+///
+/// The smart pointer also maintains a boolean flag indicating whether the allocated value has been
+/// dropped, which [`AllocMut`] wrappers rely on to perform safety checks.
+///
+/// [`AllocMut`]: ../struct.AllocMut.html
 struct ArenaBox {
+    /// Pointer to the allocated value.
     object: NonNull<u8>,
+
+    /// The function used for dropping the allocated value.
     dropper: Box<dyn FnMut()>,
+
+    /// A boolean flag indicating whether the allocated value has been dropped.
     dropped: Rc<Cell<bool>>,
 }
 
 impl ArenaBox {
+    /// Allocate and initialize a value of type `T` and create an `ArenaBox` value referencing to
+    /// the allocated value.
     fn new<T>(value: T) -> Self {
         // Allocate memory suitable for holding a value of type `T`.
         let object =
@@ -176,6 +239,7 @@ impl ArenaBox {
         }
     }
 
+    /// Set the internal dropped flag.
     fn mark_as_dropped(&self) {
         self.dropped.set(true);
     }
@@ -192,88 +256,96 @@ impl Drop for ArenaBox {
 mod tests {
     use super::*;
 
-    #[test]
-    fn test_alloc() {
-        let arena = Arena::new();
-        let value = arena.alloc(10);
-        assert_eq!(*value.get(), 10);
+    mod arena_tests {
+        use super::*;
 
-        let value = arena.alloc(20);
-        assert_eq!(*value.get(), 20);
-    }
+        #[test]
+        fn test_alloc() {
+            let arena = Arena::new();
+            let value = arena.alloc(10);
+            assert_eq!(*value.get(), 10);
 
-    #[test]
-    fn test_alloc_unsafe() {
-        let arena = Arena::new();
-        let value = unsafe { arena.alloc_unchecked(10) };
-        assert_eq!(*value, 10);
-
-        let value = unsafe { arena.alloc_unchecked(20) };
-        assert_eq!(*value, 20);
-    }
-
-    #[test]
-    fn test_drop_empty_arena() {
-        let _arena = Arena::new();
-    }
-
-    #[test]
-    fn test_drop_basic() {
-        struct Mock<'a> {
-            data: i32,
-            output: &'a RefCell<Vec<i32>>,
+            let value = arena.alloc(20);
+            assert_eq!(*value.get(), 20);
         }
 
-        impl<'a> Drop for Mock<'a> {
-            fn drop(&mut self) {
-                self.output.borrow_mut().push(self.data);
+        #[test]
+        fn test_alloc_unsafe() {
+            let arena = Arena::new();
+            let value = unsafe { arena.alloc_unchecked(10) };
+            assert_eq!(*value, 10);
+
+            let value = unsafe { arena.alloc_unchecked(20) };
+            assert_eq!(*value, 20);
+        }
+
+        #[test]
+        fn test_drop_empty_arena() {
+            let _arena = Arena::new();
+        }
+
+        #[test]
+        fn test_drop() {
+            struct Mock<'a> {
+                data: i32,
+                output: &'a RefCell<Vec<i32>>,
             }
-        }
 
-        let output = RefCell::new(Vec::new());
-        let arena = Arena::new();
-        arena.alloc(Mock {
-            data: 10,
-            output: &output,
-        });
-        arena.alloc(Mock {
-            data: 20,
-            output: &output,
-        });
-
-        drop(arena);
-
-        let output = output.borrow().clone();
-        assert_eq!(output, alloc::vec![10, 20]);
-    }
-
-    #[test]
-    #[should_panic]
-    fn test_use_dropped_value() {
-        struct Mock<'a> {
-            data: i32,
-            another: Option<AllocMut<'a, Mock<'a>>>,
-        }
-
-        impl<'a> Drop for Mock<'a> {
-            fn drop(&mut self) {
-                if let Some(another) = &mut self.another {
-                    another.data = 0;
+            impl<'a> Drop for Mock<'a> {
+                fn drop(&mut self) {
+                    self.output.borrow_mut().push(self.data);
                 }
             }
+
+            let output = RefCell::new(Vec::new());
+            let arena = Arena::new();
+            arena.alloc(Mock {
+                data: 10,
+                output: &output,
+            });
+            arena.alloc(Mock {
+                data: 20,
+                output: &output,
+            });
+
+            drop(arena);
+
+            let output = output.borrow().clone();
+            assert_eq!(output, alloc::vec![10, 20]);
         }
+    }
 
-        let arena = Arena::new();
-        let first = arena.alloc(Mock {
-            data: 10,
-            another: None,
-        });
-        arena.alloc(Mock {
-            data: 20,
-            another: Some(first),
-        });
+    mod alloc_mut_tests {
+        use super::*;
 
-        // The following statement should panic.
-        drop(arena);
+        #[test]
+        #[should_panic]
+        fn test_use_dropped_value() {
+            struct Mock<'a> {
+                data: i32,
+                another: Option<AllocMut<'a, Mock<'a>>>,
+            }
+
+            impl<'a> Drop for Mock<'a> {
+                fn drop(&mut self) {
+                    if let Some(another) = &mut self.another {
+                        another.data = 0;
+                    }
+                }
+            }
+
+            let arena = Arena::new();
+            let first = arena.alloc(Mock {
+                data: 10,
+                another: None,
+            });
+            arena.alloc(Mock {
+                data: 20,
+                another: Some(first),
+            });
+
+            // The following statement should panic.
+            drop(arena);
+        }
     }
 }
